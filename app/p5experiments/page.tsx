e2175c2a0c7aa9fdbdc9a0ef256737de901d8af7
georgegausden <george.philip.gausden@gmail.com>
2024-01-31 15:12:19 -0500

    Refactor p5Experiments component and add new sketches

--- app/p5experiments/page.tsx
+++ app/p5experiments/page.tsx
@@ -2,39 +2,7 @@
 
 import { useState, useEffect } from 'react';
 
-function TypingText({ text, speed }: { text: string, speed: number }) {
-    const [output, setOutput] = useState('');
-    const [words, setWords] = useState(initialBankOfWords);
-    let index = 0;
-    let deleting = false;
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (!deleting && index < text.length) {
-                setOutput((prevOutput) => prevOutput + text.charAt(index));
-                index++;
-            } else if (!deleting && index >= text.length) {
-                deleting = true;
-                index--;
-            } else if (deleting && text.charAt(index) === ' ') {
-                deleting = false;
-                if (words.length === 0) {
-                    setWords(initialBankOfWords);
-                }
-                setOutput((prevOutput) => prevOutput + words[0]);
-                setWords((prevWords) => prevWords.slice(1));
-            } else if (deleting) {
-                setOutput((prevOutput) => prevOutput.slice(0, -1));
-                index--;
-            } else {
-                clearInterval(interval);
-            }
-        }, speed);
-        return () => clearInterval(interval);
-    }, [text, speed, words, initialBankOfWords]);
-
-    return <p>{output}</p>;
-}
+    
 
 const initialBankOfWords = [
     'ideas',
@@ -49,10 +17,23 @@ function p5Experiments(){
     return(
         <div>
             <h1>p5 experiments</h1>
+            <p>{`From time to time, I like to experiment with p5... Here are some of my sketches.`}</p>
 
-            <TypingText text={`From time to time, I like to experiment with p5... Here are some of my ${initialBankOfWords[0]}.`} speed={100} />
 
-            <iframe src="https://editor.p5js.org/georgegausden/full/i_k7ko-wa" className="w-full h-[100em] scale-[25%]"></iframe>
+            <div className='grid grid-cols-2'>
+                
+                <iframe  loading="lazy" width="100%" height="1080px" src="https://editor.p5js.org/georgegausden/full/GccEJwSzc"></iframe>
+                <div className='text-center'>
+                    <h2>Sketch 1</h2>
+                    <p>{`This sketch involved...`}</p>
+                </div>
+               
+                
+                <iframe loading="lazy" width="100%" height="1080px" src="https://editor.p5js.org/georgegausden/full/i_k7ko-wa"></iframe>
+
+                
+
+            </div>
         </div>
     )
 
