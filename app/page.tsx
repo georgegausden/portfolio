ee59c173d076ab55c0aaa2d85c1a118d42b345f5
George Gausden <george.philip.gausden@gmail.com>
2024-02-12 21:59:07 -0500

    Refactor topic colors and add hover effect

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ import React, { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
   const [topics, setTopics] = useState(() => {
-    const topic1 = new Topic("Experiments", "/experiments", 0, 0, 10, 10, Math.random()*10, Math.random()*10);
-    const topic2 = new Topic("Contact", "/experiments", 0, 400, 10, 10, Math.random()*10, Math.random()*10);
-    const topic3 = new Topic("About", "/experiments", 150, 0, 10, 10, Math.random()*10, Math.random()*10);
+    const topic1 = new Topic("Experiments", "/experiments", 0, 0, 10, 10, Math.random()*10, Math.random()*10, "#8C9F85");
+    const topic2 = new Topic("Contact", "/experiments", 0, 400, 10, 10, Math.random()*10, Math.random()*10, "#C7D4E6");
+    const topic3 = new Topic("About", "/experiments", 150, 0, 10, 10, Math.random()*10, Math.random()*10, '#F1E7DD');
     return [topic1, topic2, topic3];
   });
 
@@ -57,9 +57,10 @@ class Topic {
   randomX: number;
   randomY: number;
   hovering: boolean = false;
+  colour: string;
 
 
-  constructor(name: string, url: string, x: number, y: number, xVelocity: number = 0, yVelocity: number = 0, randomX: number = Math.random(), randomY: number = Math.random()) {
+  constructor(name: string, url: string, x: number, y: number, xVelocity: number = 0, yVelocity: number = 0, randomX: number = Math.random(), randomY: number = Math.random(), colour: string) {
     this.name = name
     this.url = url;
     this.x = x;
@@ -71,6 +72,7 @@ class Topic {
     this.randomX = randomX;
     this.randomY = randomY;
     this.hovering = false;
+    this.colour = colour;
   }
 
   //display the topic inside of a box
@@ -82,14 +84,20 @@ class Topic {
       <div>
 
         <svg className="w-full h-screen absolute" style={{ pointerEvents: "none" }}>
-          <line x1={this.centerX} y1={this.centerY} x2={this.x.toString()} y2={this.y.toString()} style={{ stroke: "black" }} />
+          <line x1={this.centerX+5} y1={this.centerY} x2={this.x.toString()} y2={this.y.toString()} style={{ stroke: "black" }} />
         </svg>
 
-        <button style={{ transform: `translate(${this.x}px, ${this.y}px)` }}
-         className="bg-black text-white w-32 hover:bg-blue-500 p-2 uppercase hover:w-64 transition-all ease-in-out duration-500"
-         onMouseEnter={() => this.hovering = true}
-         
-         >{this.name}</button>
+        <button
+          style={{ 
+            transform: `translate(${this.x}px, ${this.y}px)`, 
+            backgroundColor: this.colour 
+          }}
+          className={`w-10 h-10 p-2 uppercase hover:w-64 transition-all ease-in-out duration-500 `}
+          onMouseEnter={() => this.hovering = true}
+          onMouseLeave={() => this.hovering = false}
+        >
+          {this.hovering && this.name}
+        </button>
 
         {/* if a boolean called hovering is triggered, display subtopics, using svgs and buttons */}
         {this.hovering && (
@@ -101,13 +109,10 @@ class Topic {
              <button style={{ transform: `translate(${this.x+100}px, ${this.y+100}px)` }}
          className="bg-black text-white w-32 hover:bg-blue-500 p-2 uppercase hover:w-64 transition-all ease-in-out duration-500"
          >{this.name}</button>
-            
+  
             </div>
-          
-
-          
-        
         )}
+
       </div>
     )
   }
@@ -116,8 +121,8 @@ class Topic {
     this.centerX = window?.innerWidth / 2;
     this.centerY = window?.innerHeight / 2;
 
-    this.x = this.centerX / 5 * Math.sin(frameCount / 5000 + this.randomX ) + this.centerX;
-    this.y = this.centerY / 5 * Math.cos(frameCount / 5000 + this.randomY) + this.centerY / 2;
+    this.x = this.centerX / 5 * Math.sin(frameCount / 500 + this.randomX ) + this.centerX;
+    this.y = this.centerY / 5 * Math.cos(frameCount / 500 + this.randomY) + this.centerY / 2;
   }
 }
 
