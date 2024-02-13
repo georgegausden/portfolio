8a8ab2251ac308dba5797e0b14447546c017b69e
georgegausden <george.philip.gausden@gmail.com>
2024-02-13 14:03:36 -0500

    Refactor topic display and add flexbox layout

--- app/page.tsx
+++ app/page.tsx
@@ -8,37 +8,61 @@ import React, { useEffect, useRef, useState } from 'react';
 
 
 export default function Home() {
-  const [topics, setTopics] = useState(() => {
-    const topic1 = new Topic("Experiments", "/experiments", 0, 0, 10, 10, Math.random()*10, Math.random()*10, "#8C9F85");
-    const topic2 = new Topic("Contact", "/experiments", 0, 400, 10, 10, Math.random()*10, Math.random()*10, "#C7D4E6");
-    const topic3 = new Topic("About", "/experiments", 150, 0, 10, 10, Math.random()*10, Math.random()*10, '#F1E7DD');
-    return [topic1, topic2, topic3];
-  });
+  // const [topics, setTopics] = useState(() => {
+  //   const topic1 = new Topic("Experiments", "/experiments", 0, 0, 10, 10, Math.random()*10, Math.random()*10, "#8C9F85");
+  //   const topic2 = new Topic("Contact", "/experiments", 0, 400, 10, 10, Math.random()*10, Math.random()*10, "#C7D4E6");
+  //   const topic3 = new Topic("About", "/experiments", 150, 0, 10, 10, Math.random()*10, Math.random()*10, '#F1E7DD');
+  //   return [topic1, topic2, topic3];
+  // });
 
-  const [frameCount, setFrameCount] = useState(0);
+  // const [frameCount, setFrameCount] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setFrameCount((prevFrameCount) => prevFrameCount + 1);
-      setTopics((topics) =>
-        topics.map((topic) => {
-          return topic;
-        })
-      );
-    }, 10);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setFrameCount((prevFrameCount) => prevFrameCount + 1);
+  //     setTopics((topics) =>
+  //       topics.map((topic) => {
+  //         return topic;
+  //       })
+  //     );
+  //   }, 10);
 
-    return () => clearInterval(interval);
-  }, [frameCount]);
+  //   return () => clearInterval(interval);
+  // }, [frameCount]);
 
-  console.log(frameCount);
+  // console.log(frameCount);
+
+  let openTopics = false;
+
+  
+
 
   return (
     <main className="w-full h-screen">
-      {topics.map((topic, index) => (
-        <div key={index}>
-          {topic.display(frameCount)}
-        </div>
-      ))}
+
+<div className="flex flex-wrap overflow-hidden bg-black">
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+ <div className="w-1/4  sm:w-full  md:w-full  lg:w-1/5 lg:px-1 lg:my-1 xl:w-full "></div>
+  </div>
+
+
     </main>
   );
 }
@@ -78,7 +102,7 @@ class Topic {
   //display the topic inside of a box
   display(frameCount: number = 0) {
 
-    this.move(frameCount);
+    // this.move(frameCount);
 
     return (
       <div>
@@ -89,7 +113,8 @@ class Topic {
 
         <button
           style={{ 
-            transform: `translate(${this.x}px, ${this.y}px)`, 
+            // transform: `translate(${this.x}px, ${this.y}px)`, 
+            transform: `translate(${this.x}px, ${this.y}px)`,
             backgroundColor: this.colour 
           }}
           className={`w-10 h-10 p-2 uppercase hover:w-64 transition-all ease-in-out duration-500 `}
@@ -99,19 +124,7 @@ class Topic {
           {this.hovering && this.name}
         </button>
 
-        {/* if a boolean called hovering is triggered, display subtopics, using svgs and buttons */}
-        {this.hovering && (
-          <div>
-            <svg className="w-full h-screen absolute" style={{ pointerEvents: "none" }}>
-            <line x1={this.x+100} y1={this.y+100} x2={this.x.toString()} y2={this.y.toString()} style={{ stroke: "black" }} />
-             </svg>
-
-             <button style={{ transform: `translate(${this.x+100}px, ${this.y+100}px)`, backgroundColor: this.colour  }}
-         className="bg-black text-white w-32 hover:bg-blue-500 p-2 uppercase hover:w-64 transition-all ease-in-out duration-500"
-         >{this.name}</button>
-  
-            </div>
-        )}
+        
 
       </div>
     )
@@ -121,8 +134,8 @@ class Topic {
     this.centerX = window?.innerWidth / 2;
     this.centerY = window?.innerHeight / 2;
 
-    this.x = this.centerX / 5 * Math.sin(frameCount / 500 + this.randomX ) + this.centerX;
-    this.y = this.centerY / 5 * Math.cos(frameCount / 500 + this.randomY) + this.centerY / 2;
+    this.x = this.centerX / 5 * Math.sin(frameCount / 50 + this.randomX ) + this.centerX;
+    this.y = this.centerY / 5 * Math.cos(frameCount / 50 + this.randomY) + this.centerY / 2;
   }
 }
 
