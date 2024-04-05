f1642ad4e6ed39417f1a802bbc5bad7d618da2e0
georgegausden <george.philip.gausden@gmail.com>
2024-04-05 13:46:43 -0400

    Update navbar links

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,7 +10,8 @@ return (
         <div className="grid grid-rows-3 text-center text-gray-800 gap-[91px] font-bold">
             <Link className="mb-0 mt-0" href="/">Home</Link>
             <Link className="mb-0 mt-0" href="/about">About</Link>
-            <Link className="mb-0 mt-0" href="/contact">Contact</Link>
+            <a className="mb-0 mt-0" href="mailto:george.philip.gausden@gmail.com">Email</a>
+            <Link className="mb-0 mt-0"  href="/CV.pdf" download>CV</Link>
             
         </div>
         {/* navigation for the experiment page */}
