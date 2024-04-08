ad3e51889eaf41db0ffdb9f07f0e1957da5c2b1d
George Gausden <george.philip.gausden@gmail.com>
2024-04-08 13:17:00 -0400

    Update mainLayout component styles

--- app/components/mainLayout.tsx
+++ app/components/mainLayout.tsx
@@ -13,7 +13,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
 
   return (
     <div id = "backgroundEffect">
-    <div className={isHomePage ? '' : 'ml-[39px] mt-[87px] relative scroll-smooth z-10 sm:mr-[39px]'}>
+    <div className={isHomePage ? '' : 'sm:ml-[39px] mt-[87px] relative scroll-smooth z-10 sm:mr-[39px] max-w-screen-xl xl:mx-auto'}>
       {children}
     </div>
     </div>
