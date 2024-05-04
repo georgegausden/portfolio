2166645b2c5893745de21ba40bc1f862def22ff4
georgegausden <george.philip.gausden@gmail.com>
2024-05-04 17:43:44 -0400

    Refactor Test component in page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,16 @@
-import Branch from "../components/Branch"
+// import Branch from "../components/Branch"
 
-export default function Test() {
+// export default function Test() {
 
 
-  return (
-    <div className="w-full h-screen bg-pink-50">
-      <Branch name="About" isActive={true} colour='#FFFFFF' xPosition={25} yPosition={25}>
-        <Branch name="Biography" isActive={false} colour='#F1E7DD' xPosition={10} yPosition={10} />
-      </Branch>
-      <Branch name="3D Projects" isActive={true} colour='#F1E7DD' xPosition={75} yPosition={25} />
-      <Branch name="Contact" isActive={true} colour='#F1E7DD' xPosition={25} yPosition={75} />
-      <Branch name="Experiments" isActive={true} colour='#F1E7DD' xPosition={75} yPosition={75} />
-    </div>
-  )
-}
+//   return (
+//     <div className="w-full h-screen bg-pink-50">
+//       <Branch name="About" isActive={true} colour='#FFFFFF' xPosition={25} yPosition={25}>
+//         <Branch name="Biography" isActive={false} colour='#F1E7DD' xPosition={10} yPosition={10} />
+//       </Branch>
+//       <Branch name="3D Projects" isActive={true} colour='#F1E7DD' xPosition={75} yPosition={25} />
+//       <Branch name="Contact" isActive={true} colour='#F1E7DD' xPosition={25} yPosition={75} />
+//       <Branch name="Experiments" isActive={true} colour='#F1E7DD' xPosition={75} yPosition={75} />
+//     </div>
+//   )
+// }
