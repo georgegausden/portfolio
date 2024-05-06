10f9dad81f5a8ae3e33a97beeabef2c66b6fbf71
georgegausden <george.philip.gausden@gmail.com>
2024-05-06 10:59:24 -0400

    Refactor Test component in page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Branch from "../components/Branch";
+// import Branch from "../components/Branch";
 import LeftHandBar from "../components/LeftHandBar";
 import RightHandBar from "../components/RightHandBar";
 import Image from "next/image";
@@ -36,7 +36,7 @@ export default function Test() {
   };
 
   return (
-    <div className="w-full flex h-screen bg-slate-200 ">
+    <div className="w-full flex h-screen  ">
       <div
         style={{ left: xPosition, top: yPosition }}
         className="absolute w-[30.5%] h-[54%] translate-y-5 translate-x-5 border-black border"
@@ -64,28 +64,28 @@ export default function Test() {
       </div>
 
       <button
-        className={`h-10 w-10 bg-[#707F6A] absolute   ${
+        className={`h-20 w-10 bg-[#707F6A] absolute   ${
           !isClicked && "animate-pulse"
         }`}
         style={{ left: xPosition, top: yPosition, animationDelay: "0s" }}
         onClick={() => handleClick("About")}
       ></button>
       <button
-        className={`h-10 w-10 bg-black absolute   ${
+        className={`h-10 w-20 bg-black absolute   ${
           !isClicked && "animate-pulse"
         }`}
         style={{ right: xPosition, top: yPosition, animationDelay: "0.35s" }}
         onClick={() => handleClick("Experiments")}
       ></button>
       <button
-        className={`h-10 w-10 bg-[#A7B6CA] absolute   ${
+        className={`h-20 w-10 bg-[#A7B6CA] absolute   ${
           !isClicked && "animate-pulse"
         }`}
         style={{ right: xPosition, bottom: yPosition, animationDelay: "0.65s" }}
         onClick={() => handleClick("Contact")}
       ></button>
       <button
-        className={`h-10 w-10 bg-[#9e968f] absolute   ${
+        className={`h-10 w-20 bg-[#9e968f] absolute   ${
           !isClicked && "animate-pulse"
         }`}
         style={{ left: xPosition, bottom: yPosition, animationDelay: "0.95s" }}
