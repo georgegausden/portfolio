cf4085edf8baeaaa6e46303d32670334c5178690
georgegausden <george.philip.gausden@gmail.com>
2024-05-06 08:57:59 -0400

    Refactor RightHandBar component to use framer-motion for animation

--- app/components/RightHandBar.tsx
+++ app/components/RightHandBar.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import projects from '../data/projects.json'
 
 type RightHandBarProps = {
@@ -13,11 +14,23 @@ export default function RightHandBar({ activeProject }: RightHandBarProps) {
 
   console.log("Active project title:", activeProject?.title);
 
-
   return(
     <div className="py-[20%] px-10">
-      <h2>{project?.title}</h2>
-      <p>{project?.description}</p>
+      <AnimatePresence mode='wait'>
+        {project && (
+          <motion.div
+            key={project.title}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+             // Add this
+          >
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,66 +1,98 @@
-'use client'
+"use client";
 import Branch from "../components/Branch";
 import LeftHandBar from "../components/LeftHandBar";
 import RightHandBar from "../components/RightHandBar";
-import Image from 'next/image'
+import Image from "next/image";
 import { SetStateAction, useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Test() {
   const [activeButton, setActiveButton] = useState<string | null>(null);
-  const [activeProject, setActiveProject] = useState<{ title: string; description: string; link: string } | null>(null);
+  const [activeProject, setActiveProject] = useState<{
+    title: string;
+    description: string;
+    link: string;
+  } | null>(null);
   const [isClicked, setIsClicked] = useState(false);
+
+  const xPosition = "33.333%";
+  const yPosition = "20%";
 
   const handleClick = (buttonInfo: string) => {
     setActiveButton(buttonInfo);
     setIsClicked(true);
-
   };
 
-  const handleProjectClick = (project: { title: string; description: string; link: string }) => {
+  const handleProjectClick = (project: {
+    title: string;
+    description: string;
+    link: string;
+  }) => {
     setActiveProject(project);
   };
-
- 
-
-
-  
 
   return (
     <div className="w-full flex h-screen bg-slate-200 ">
       <div
-        style={{ left: "33.333%", top: "20%" }}
+        style={{ left: xPosition, top: yPosition }}
         className="absolute w-[30.5%] h-[54%] translate-y-5 translate-x-5 border-black border"
       >
-
         {/* here is where the image or video of the project goes */}
-        {activeProject?.link && <Image src={activeProject.link} alt="project" layout="fill" objectFit="contain" className="-translate-x-10 -translate-y-10 scale-[90%]"  />}
+        <AnimatePresence mode="wait">
+          {activeProject?.link && (
+            <motion.div
+              key={activeProject.link}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <Image
+                src={activeProject.link}
+                alt="project"
+                layout="fill"
+                objectFit="contain"
+                className="-translate-x-10 -translate-y-10 scale-[90%]"
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>{" "}
       </div>
 
       <button
-      className={`h-10 w-10 bg-[#707F6A] absolute   ${!isClicked && 'animate-pulse'}`}
-      style={{ left: "33.333%", top: "20%", animationDelay: '0s' }}
-      onClick = {() => handleClick('About')}
+        className={`h-10 w-10 bg-[#707F6A] absolute   ${
+          !isClicked && "animate-pulse"
+        }`}
+        style={{ left: xPosition, top: yPosition, animationDelay: "0s" }}
+        onClick={() => handleClick("About")}
       ></button>
       <button
-      className={`h-10 w-10 bg-black absolute   ${!isClicked && 'animate-pulse'}`}
-      style={{ right: "33.333%", top: "20%", animationDelay: '0.35s' }}
-      onClick = {() => handleClick('Experiments')}
+        className={`h-10 w-10 bg-black absolute   ${
+          !isClicked && "animate-pulse"
+        }`}
+        style={{ right: xPosition, top: yPosition, animationDelay: "0.35s" }}
+        onClick={() => handleClick("Experiments")}
       ></button>
       <button
-      
-      className={`h-10 w-10 bg-[#A7B6CA] absolute   ${!isClicked && 'animate-pulse'}`}
-      style={{ right: "33.333%", bottom: "20%", animationDelay: '0.65s' }}
-      onClick = {() => handleClick('Contact')}
+        className={`h-10 w-10 bg-[#A7B6CA] absolute   ${
+          !isClicked && "animate-pulse"
+        }`}
+        style={{ right: xPosition, bottom: yPosition, animationDelay: "0.65s" }}
+        onClick={() => handleClick("Contact")}
       ></button>
       <button
-      
-      className={`h-10 w-10 bg-[#9e968f] absolute   ${!isClicked && 'animate-pulse'}`}
-      style={{ left: "33.333%", bottom: "20%", animationDelay: '0.95s' }}
-      onClick = {() => handleClick('3d')}
+        className={`h-10 w-10 bg-[#9e968f] absolute   ${
+          !isClicked && "animate-pulse"
+        }`}
+        style={{ left: xPosition, bottom: yPosition, animationDelay: "0.95s" }}
+        onClick={() => handleClick("3d")}
       ></button>
 
       <div className="w-1/3 h-full">
-      <LeftHandBar activeButton = {activeButton || ""} onProjectClick={handleProjectClick || ""}/>
+        <LeftHandBar
+          activeButton={activeButton || ""}
+          onProjectClick={handleProjectClick || ""}
+        />
       </div>
 
       {/* empty div to make space */}
@@ -69,7 +101,6 @@ export default function Test() {
       <div className="w-1/3 h-full ">
         <RightHandBar activeProject={activeProject ? activeProject : null} />
       </div>
-
     </div>
   );
 }
